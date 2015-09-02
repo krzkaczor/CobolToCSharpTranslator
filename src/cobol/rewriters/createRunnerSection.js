60d9e82d7f51bbe4a612edaca3075335f724c97d
Krzysztof Kaczor <krzkaczor@gmail.com>
2015-09-02 02:35:11 +0200

    create runner rewritter with tests

--- src/cobol/rewriters/createRunnerSection.js
+++ src/cobol/rewriters/createRunnerSection.js
@@ -1,5 +1,8 @@
 var _ = require('lodash');
 
+var utils = require('../../utils');
+var nodes = require('../nodes');
+
 /**
  * Creates runner section
  * if it exitsts and it's first then it does nothing
@@ -11,6 +14,15 @@ module.exports = function(runnerSectionName, compilationUnit) {
     var sections = compilationUnit.procedureDivision.sections;
 
     if (_.chain(sections).filter(section => section.name === runnerSectionName).any().value()) {
-        throw new Error('name conflict');
+        var conflictingSection = sections.filter(section => section.name === runnerSectionName)[0];
+        var allSectionsNames = sections.map(section => section.name);
+
+        var uniqueNewName = utils.makeUniqueInSet(conflictingSection.name, allSectionsNames);
+
+        debugger;
+        //TODO: it should be properly renamed!
+        conflictingSection.name = uniqueNewName;
     }
+
+    compilationUnit.procedureDivision.sections.unshift(new nodes.Section('Runner'));
 };
--- test/unit/rewriters/createRunnerSection.spec.js
+++ test/unit/rewriters/createRunnerSection.spec.js
@@ -4,19 +4,64 @@ var expect = require('chai').expect;
 var appRequire = require('../../testHelper').appRequire;
 
 var createRunnerSectionRewritter = appRequire('cobol/rewriters/createRunnerSection.js');
+var nodes = appRequire('cobol/nodes');
 
 describe.only('Create runner section rewriter', function () {
+    it('should add runner', function () {
+        var otherSection = {
+            name:'OtherSection'
+        };
+
+        var cobolProgram = {
+            procedureDivision: {
+                sections: [
+                    otherSection
+                ]
+            }
+        };
+
+        createRunnerSectionRewritter('Runner', cobolProgram);
+
+        expect(cobolProgram.procedureDivision.sections[0]).to.be.eql(new nodes.Section('Runner'));
+        expect(cobolProgram.procedureDivision.sections[1]).to.be.eql(otherSection);
+    });
+
     it('should detect name conflict', function () {
+        var otherSection = {
+            name:'Runner'
+        };
+
+        var cobolProgram = {
+            procedureDivision: {
+                sections: [
+                    otherSection
+                ]
+            }
+        };
+
+        createRunnerSectionRewritter('Runner', cobolProgram);
+
+        expect(cobolProgram.procedureDivision.sections[0]).to.be.eql(new nodes.Section('Runner'));
+        expect(cobolProgram.procedureDivision.sections[1]).to.be.eql({name: '_Runner'});
+    });
+
+    it('should detect double name conflict', function () {
         var cobolProgram = {
             procedureDivision: {
                 sections: [
                     {
-                        name:'Runner'
+                        name: 'Runner'
+                    },
+                    {
+                        name: '_Runner'
                     }
                 ]
             }
         };
 
         createRunnerSectionRewritter('Runner', cobolProgram);
+
+        expect(cobolProgram.procedureDivision.sections[0]).to.be.eql(new nodes.Section('Runner'));
+        expect(cobolProgram.procedureDivision.sections[1]).to.be.eql({name: '__Runner'});
     });
 });
