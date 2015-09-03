ef2da537a73038f42ffb478fe4f5a46ffde4208a
Krzysztof Kaczor <krzkaczor@gmail.com>
2015-09-03 10:34:25 +0200

    runner

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const SAMPLES_PATH = path.join(__dirname, '../samples/');
 
 var cobolProgram = fs.readFileSync(path.join(SAMPLES_PATH, 'sectionsHelloWorld.cob')).toString();
 
-var cobolAst = new CobolToCSharpTranslator().getCobolAst(cobolProgram);
-console.log(require('util').inspect(cobolAst, true, 10));
+//var cobolAst = new CobolToCSharpTranslator().getCobolAst(cobolProgram);
+//console.log(require('util').inspect(cobolAst, false, 10));
 
-//var csharpAst = new CobolToCSharpTranslator().getCSharpAst(cobolProgram);
-//console.log(require('util').inspect(csharpAst, true, 10));
-//
-//console.log(csharpAst.toSource());
-////console.log(new CobolToCSharpTranslator().emitCSharp(cobolProgram));
+//var cobolAst = new CobolToCSharpTranslator().getCobolAstAndRewrite(cobolProgram);
+//console.log(require('util').inspect(cobolAst, false, 10));
+
+var csharpAst = new CobolToCSharpTranslator().getCSharpAst(cobolProgram);
+//console.log(require('util').inspect(csharpAst, false, 10));
+
+console.log(csharpAst.toSource());
