8d1b740b076b67af945b1ba507c16af7030f63c6
Krzysztof Kaczor <krzkaczor@gmail.com>
2015-10-11 12:16:49 +0200

    more verbose debug script

--- debug-antlr.sh
+++ debug-antlr.sh
@@ -9,11 +9,12 @@ fi
 
 INPUT_FILE="$(pwd)/$INPUT_FILE"
 
-echo "using file: $INPUT_FILE"
+echo "using input file: $INPUT_FILE"
+echo "tmp dir: /tmp/antlr-debug/"
 #clean
 rm -r /tmp/antlr-debug
 cd src/cobol/grammar/
-java -Xmx500M -cp "/usr/local/lib/antlr-4.5-complete.jar:$CLASSPATH" org.antlr.v4.Tool -o /tmp/antlr-debug/ Cobol85.g4
+java -Xmx500M -cp "/usr/local/lib/antlr-4.5-complete.jar:$CLASSPATH" org.antlr.v4.Tool -visitor -o /tmp/antlr-debug/ Cobol85.g4
 cd /tmp/antlr-debug
 javac Cobol85*.java
 java org.antlr.v4.runtime.misc.TestRig Cobol85 compilationUnit -gui < $INPUT_FILE
