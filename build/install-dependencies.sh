b73fde17d2350ca34bec828ba1f17cc10c8da2d3
Krzysztof Kaczor <krzkaczor@gmail.com>
2015-10-13 22:32:07 +0200

    further fixes for build script

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /usr/local/lib
-sudo wget http://www.antlr.org/download/antlr-4.5.1-complete.jar
+sudo wget http://www.antlr.org/download/antlr-4.5-complete.jar
 export CLASSPATH=".:/usr/local/lib/antlr-4.5.1-complete.jar:$CLASSPATH"
