087a920951095a59908352132e950a8dd0f70590
Krzysztof Kaczor <krzkaczor@gmail.com>
2015-10-18 20:02:26 +0200

    installing newest mono (4), pimped build scripts

--- build/install-antlr.sh
+++ build/install-antlr.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /usr/local/lib
-sudo wget http://www.antlr.org/download/antlr-4.5-complete.jar
+sudo curl -O http://www.antlr.org/download/antlr-4.5.1-complete.jar
 export CLASSPATH=".:/usr/local/lib/antlr-4.5.1-complete.jar:$CLASSPATH"
